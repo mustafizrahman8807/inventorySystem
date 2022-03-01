e3b76292039d3f430c8b2aae7958dbb14768b9a4
Mustafijur Rahman <mustafizrahman8807@gmail.com>
2022-03-01 07:14:26 +0000

    First commit

---
+++ inventorySystem/inventory/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    
+    context = {
+        "title": "Index Page"
+    }
+    
+    return render(request, 'inventory/index.html', context=context)
